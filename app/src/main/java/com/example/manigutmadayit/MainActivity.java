53ced850c515bb00fc2de4f46fdb657cc227b2b6
overp <overpoweredronen@gmail.com>
2024-03-24 17:49:11 +0000

    Added events + delete events + update events + search for events

--- app/src/main/java/com/example/manigutmadayit/MainActivity.java
+++ app/src/main/java/com/example/manigutmadayit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.manigutmadayit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,13 +9,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseAuth auth;
+    Button events;
     Button button;
     Button branch;
     Button substitute;
@@ -27,8 +34,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //FirebaseDatabase database = FirebaseDatabase.getInstance("https://manigut-madayit-default-rtdb.europe-west1.firebasedatabase.app");
+        //DatabaseReference myRef = database.getReference("message");
         auth = FirebaseAuth.getInstance();
+        events = findViewById(R.id.specDay);
         button = findViewById(R.id.logout);
         textView = findViewById(R.id.loggedIn);
         sharedprefs = findViewById(R.id.SharedPrefs);
@@ -64,6 +73,14 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+        /*events.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Events.class);
+                startActivity(intent);
+                finish();
+            }
+        });*/
         sharedprefs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,6 +99,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 Log.d("bcaddd","hii");
                 finish();
+                //myRef.setValue("hello");
+                //Log.d("DB","sent");
             }
         });
         lessons.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/manigutmadayit/UploadActivity.java
+++ app/src/main/java/com/example/manigutmadayit/UploadActivity.java
@@ -1,6 +1,13 @@
 package com.example.manigutmadayit;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,15 +16,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -26,11 +24,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-
-
+import java.text.DateFormat;
+import java.util.Calendar;
 public class UploadActivity extends AppCompatActivity {
-
-
     ImageView uploadImage;
     Button saveButton;
     EditText uploadTopic, uploadDesc, uploadLang;
@@ -40,13 +36,11 @@ public class UploadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
-
         uploadImage = findViewById(R.id.uploadImage);
-        saveButton = findViewById(R.id.saveButton);
-        uploadTopic = findViewById(R.id.uploadTopic);
         uploadDesc = findViewById(R.id.uploadDesc);
+        uploadTopic = findViewById(R.id.uploadTopic);
         uploadLang = findViewById(R.id.uploadLang);
-
+        saveButton = findViewById(R.id.saveButton);
         ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
@@ -56,52 +50,35 @@ public class UploadActivity extends AppCompatActivity {
                             Intent data = result.getData();
                             uri = data.getData();
                             uploadImage.setImageURI(uri);
-                        }
-                        else{
-                            Toast.makeText(UploadActivity.this,"No image Selected",Toast.LENGTH_SHORT).show();
-
+                        } else {
+                            Toast.makeText(UploadActivity.this, "No Image Selected", Toast.LENGTH_SHORT).show();
                         }
                     }
-                });
-
+                }
+        );
         uploadImage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent photoPicker = new Intent(Intent.ACTION_PICK);
                 photoPicker.setType("image/*");
                 activityResultLauncher.launch(photoPicker);
             }
         });
-
-
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 saveData();
             }
         });
-
-
-
-
-
-
-
-
-
-
     }
-
-    private void saveData() {
+    public void saveData(){
         StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("Android Images")
                 .child(uri.getLastPathSegment());
-
         AlertDialog.Builder builder = new AlertDialog.Builder(UploadActivity.this);
         builder.setCancelable(false);
         builder.setView(R.layout.progress_layout);
         AlertDialog dialog = builder.create();
         dialog.show();
-
         storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
             public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
@@ -123,22 +100,26 @@ public class UploadActivity extends AppCompatActivity {
         String title = uploadTopic.getText().toString();
         String desc = uploadDesc.getText().toString();
         String lang = uploadLang.getText().toString();
-
         DataClass dataClass = new DataClass(title, desc, lang, imageURL);
-
-        FirebaseDatabase.getInstance().getReference("Manigut").child(title).setValue(dataClass)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(UploadActivity.this,"saved",Toast.LENGTH_SHORT).show();
-                    finish();
-                }
-            }
-        }).addOnFailureListener(new OnFailureListener() {
+        //We are changing the child from title to currentDate,
+        // because we will be updating title as well and it may affect child value.
+        String currentDate = DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime());
+        FirebaseDatabase.getInstance("https://manigut-madayit-default-rtdb.europe-west1.firebasedatabase.app").getReference("test").child(currentDate)
+                .setValue(dataClass).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(UploadActivity.this, "Saved", Toast.LENGTH_SHORT).show();
+                            /*Intent intent = new Intent(UploadActivity.this, RealTimeDataBase.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);*/
+                            finish();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(UploadActivity.this,e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(UploadActivity.this, e.getMessage().toString(), Toast.LENGTH_SHORT).show();
                     }
                 });
     }
